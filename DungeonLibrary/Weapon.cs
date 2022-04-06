8237d17228cf6c8e7e84928b4f2417f0bb585658
Lance Johnson <lancejohnson2009@gmail.com>
2022-04-05 20:57:52 -0500

    created dungeon library and datatypes

---
+++ DungeonLibrary/Weapon.cs
@@ -0,0 +1,57 @@
+﻿namespace DungeonLibrary
+{
+    public class Weapon
+    {
+        //FIELDS
+        private int _minDamage;
+        private int _maxDamage;
+        private string _name;
+        private int _bonusHitChance;
+        private bool _isTwoHanded;
+
+        //PROPERTIES
+
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set { _minDamage = value; }
+        }
+        public int MaxDamage
+        {
+            get { return _maxDamage; }
+            set { _maxDamage = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int BonusHitChance
+        {
+            get { return _bonusHitChance; }
+            set { _bonusHitChance = value; }
+        }
+        public bool IsTwoHanded
+        {
+            get { return _isTwoHanded; }
+            set { _isTwoHanded = value; }
+        }
+
+        //CONSTRUCTORS
+
+        public Weapon(int minDamage, int maxDamage, string name, int bonusHitChance, bool isTwoHanded)
+        {
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+            Name = name;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;
+        }
+
+        //METHODS
+        public override string ToString()
+        {
+            return string.Format($"Min Damage: {MinDamage}\nMaxDamage: {MaxDamage}\nName {Name}\n Bonuse Hit Change {BonusHitChance}\nIsTwoHanded {IsTwoHanded}");
+        }
+    }
+}
